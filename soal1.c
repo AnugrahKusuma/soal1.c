7766b73bbc32dacf5fc313b05fcd13c10e03dc22
AnugrahKusuma <anugrah080820@gmail.com>
2024-09-09 19:37:27 +0800

    Add files via upload

---
+++ soal1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int nilai;
+    scanf("%d", &nilai); 
+        if (nilai %2==0){
+            printf("Genap");
+        
+        }else{
+            printf("Ganjil");
+        }
+}
+git remote add origin https://github.com/AnugrahKusuma/soal1.c.git
+git branch -M main
+git push -u origin main
